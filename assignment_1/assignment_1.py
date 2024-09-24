395aecd78f805ead3245ea3f9d919d8b654c72ae
Dasar0410 <danielsarjomaa@gmail.com>
2024-09-24 11:25:16 +0200

    lagt til duplikat og null verdi sjekk

--- assignment_1/assignment_1.py
+++ assignment_1/assignment_1.py
@@ -3,4 +3,6 @@ import pandas as pd
 df = pd.read_csv('OnlineNewsPopularity.csv')
 df.info()
 
+#print(df.isnull().sum()) #ingen null verdier
+#print(df.duplicated().sum()) #ingen duplikater
 #print(df.to_string()) #printer ut alt be careful
